2bddd9f5e3b986bcd202e46bcfef964221a1eb7b
nyagaskate <greatdane_sparrow@yahoo.co.jp>
2026-04-24 22:29:28 +0900

    Set up React rendering for SkateClub component

--- skate-club.jsx
+++ skate-club.jsx
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SkateClub from './skate-club';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<SkateClub />);
