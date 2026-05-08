0e038988f9fb315e678ce005c2b39c65d2de9c4b
nyagaskate <greatdane_sparrow@yahoo.co.jp>
2026-05-08 16:03:34 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './skate-club';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+createRoot(document.getElementById('root')).render(<App />);
